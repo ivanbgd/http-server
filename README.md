b4460febbf3120c7055b16023942a4b7eca25758
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-12-06 10:29:23 +0100

    Refactor handlers

--- README.md
+++ README.md
@@ -41,7 +41,8 @@ Note: This section is for stages 2 and beyond.
 
 - If you would like to enable the added logging functionality, first set the `RUST_LOG` environment variable.
     - `export RUST_LOG=[trace | debug | info | warn]`
-- Run `./your_server.sh` in one terminal session, and `curl -v http://localhost:4221` in another.
+- Run `./your_server.sh` in one terminal session, and `curl -v http://localhost:4221`
+  or some other network tool in another.
 
 # Notes
 
--- src/conn.rs
+++ src/conn.rs
@@ -1,10 +1,11 @@
-//! Connection facility
+//! Connection and request handlers
 
 use crate::constants::{
     BUFFER_LEN, GET_ECHO_URI, GET_ROOT_URI, GET_USER_AGENT_URI, STATUS_200_OK, STATUS_404_NOT_FOUND,
 };
 use crate::errors::ConnectionError;
 use crate::templates::{echo_html, hello_html, not_found_404_html};
+use anyhow::Result;
 use httparse;
 use httparse::{parse_headers, Status};
 use log::{trace, warn};
@@ -12,7 +13,7 @@ use std::io::BufRead;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
-pub async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<(), ConnectionError> {
+pub async fn handle_connection(mut stream: TcpStream) -> Result<(), ConnectionError> {
     trace!("Start handling request from {}", stream.peer_addr()?);
 
     let mut buf = [0; BUFFER_LEN];
@@ -25,49 +26,15 @@ pub async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<(), Conn
         }
     }
 
-    let mut headers = [httparse::EMPTY_HEADER; 8];
-
-    let (status_line, contents, length) = if buf.starts_with(GET_ROOT_URI) {
-        let contents = hello_html();
-        let length = contents.len();
-        (STATUS_200_OK, contents, length)
+    let response = if buf.starts_with(GET_ROOT_URI) {
+        get_root()?
     } else if buf.starts_with(GET_ECHO_URI) {
-        let rest = buf
-            .strip_prefix(GET_ECHO_URI)
-            .ok_or(ConnectionError::ParseError("strip echo prefix".to_string()))?;
-        let line = rest.lines().next().unwrap()?;
-        let echo = line
-            .strip_suffix(" HTTP/1.1")
-            .ok_or(ConnectionError::ParseError("strip echo suffix".to_string()))?;
-        let contents = echo_html(echo);
-        let length = contents.len();
-        (STATUS_200_OK, contents, length)
+        get_echo(&buf)?
     } else if buf.starts_with(GET_USER_AGENT_URI) {
-        let line = buf.lines().next().unwrap()?;
-        trace!("{}", line);
-        let rest = &buf[line.len() + 2..];
-        trace!("{}", String::from_utf8_lossy(rest).replace("\r\n", " "));
-        while let Status::Partial = parse_headers(rest, &mut headers)? {}
-        let parsed = parse_headers(rest, &mut headers)?.unwrap();
-        trace!("Request body begins at byte index {:?}.", parsed.0);
-        let contents = parsed
-            .1
-            .iter()
-            .find(|&&h| h.name.to_lowercase() == "User-Agent".to_lowercase())
-            .ok_or(ConnectionError::UserAgentMissing)?
-            .value;
-        let contents = String::from_utf8_lossy(contents).to_string();
-        let length = contents.len();
-        (STATUS_200_OK, contents, length)
+        get_user_agent(&buf)?
     } else {
-        let contents = not_found_404_html();
-        let length = contents.len();
-        (STATUS_404_NOT_FOUND, contents, length)
+        get_not_found()?
     };
-
-    let header =
-        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n");
-    let response = format!("{header}{contents}");
 
     stream.write_all(response.as_bytes()).await?;
     stream.flush().await?;
@@ -75,4 +42,83 @@ pub async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<(), Conn
     trace!("Stop handling request from {}", stream.peer_addr()?);
 
     Ok(())
+}
+
+/// `GET / HTTP/1.1`
+///
+/// - `HTTP/1.1 200 OK`
+fn get_root() -> Result<String> {
+    let contents = hello_html();
+    let length = contents.len();
+    let status_line = STATUS_200_OK;
+    let header =
+        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n");
+    let response = format!("{header}{contents}");
+    Ok(response)
+}
+
+/// `GET /echo/{text} HTTP/1.1`
+///
+/// - `HTTP/1.1 200 OK`
+/// - `Content-Type: text/plain`
+/// - `Content-Length: <text_length>`
+/// - `text`
+fn get_echo(buf: &[u8]) -> Result<String> {
+    let rest = buf
+        .strip_prefix(GET_ECHO_URI)
+        .ok_or(ConnectionError::ParseError("strip echo prefix".to_string()))?;
+    let line = rest.lines().next().unwrap()?;
+    let echo = line
+        .strip_suffix(" HTTP/1.1")
+        .ok_or(ConnectionError::ParseError("strip echo suffix".to_string()))?;
+    let contents = echo_html(echo);
+    let length = contents.len();
+    let status_line = STATUS_200_OK;
+    let header =
+        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n");
+    let response = format!("{header}{contents}");
+    Ok(response)
+}
+
+/// `GET /echo/user-agent HTTP/1.1`
+///
+/// - `HTTP/1.1 200 OK`
+/// - `Content-Type: text/plain`
+/// - `Content-Length: <user-agent_length>`
+/// - `"User-Agent" value`
+fn get_user_agent(buf: &[u8]) -> Result<String> {
+    let line = buf.lines().next().unwrap()?;
+    trace!("{}", line);
+    let rest = &buf[line.len() + 2..];
+    trace!("{}", String::from_utf8_lossy(rest).replace("\r\n", " "));
+    let mut headers = [httparse::EMPTY_HEADER; 8];
+    while let Status::Partial = parse_headers(rest, &mut headers)? {}
+    let parsed = parse_headers(rest, &mut headers)?.unwrap();
+    trace!("Request body begins at byte index {:?}.", parsed.0);
+    let contents = parsed
+        .1
+        .iter()
+        .find(|&&h| h.name.to_lowercase() == "User-Agent".to_lowercase())
+        .ok_or(ConnectionError::UserAgentMissing)?
+        .value;
+    let contents = String::from_utf8_lossy(contents).to_string();
+    let length = contents.len();
+    let status_line = STATUS_200_OK;
+    let header =
+        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n");
+    let response = format!("{header}{contents}");
+    Ok(response)
+}
+
+/// `GET /{non-existent} HTTP/1.1`
+///
+/// - `HTTP/1.1 404 Not Found`
+fn get_not_found() -> Result<String> {
+    let contents = not_found_404_html();
+    let length = contents.len();
+    let status_line = STATUS_404_NOT_FOUND;
+    let header =
+        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n");
+    let response = format!("{header}{contents}");
+    Ok(response)
 }
